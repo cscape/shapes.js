9726c00cba3463ff0981a29f590d61c59d64e650
mausworks <rasmuswennerstrom@gmail.com>
2018-07-24 16:21:45 +0200

    Core files

---
+++ src/shapes.d.ts
@@ -0,0 +1,157 @@
+/**
+ * - A DOMString parsed as CSS <color> value.
+ * - A CanvasGradient object (a linear or radial gradient).
+ * - A CanvasPattern object (a repetitive image).
+ */
+type ShapeStyle = string | CanvasGradient | CanvasPattern;
+
+/** The object is stroked. */
+interface Stroked {
+  /** The thickness of the stroke, in pixels */
+  thickness: number;
+}
+
+/** The object is offset. */
+interface Offset {
+  /** Number of pixels offset from the left */
+  x: number;
+  /** Number of pixels offset from the top */
+  y: number;
+}
+
+/** Defines the properties of a drawable rectangle. */
+interface RectDef extends Offset {
+  /** The width of the rectangle in pixels */
+  width: number;
+  /** The height of the rectangle in pixels */
+  height: number;
+  /** The style to apply to the rectangle */
+  style: ShapeStyle;
+}
+
+/** Describes the properties of drawable text. */
+interface TextDef extends Offset {
+  /** The text to render */
+  value: string;
+  /**
+   * A DOMString parsed as CSS font value.
+   * The default font is 10px sans-serif.
+   */
+  font: string;
+  /** The style to render the text in */
+  style: ShapeStyle;
+}
+
+/** Defines the properties of a drawable polygon. */
+interface PolyDef {
+  /** A nested array consisting of `[ x, y ]`-coordinates. */
+  points: number[];
+  /** The style to draw the polygon in.  */
+  style: ShapeStyle;
+}
+
+/** Describes the properties of a drawable path.  */
+interface PathDef {
+  /** The Path2D to draw to the context. */
+  path: Path2D;
+  /** The style to draw the path in. */
+  style: ShapeStyle;
+}
+
+/** Describes the properties of a circle. */
+interface CircleDef extends Offset {
+  /** The radius of the circle. */
+  radius: number;
+}
+
+/** Defines the definition of any shape. */
+type ShapeDef = PathDef | PolyDef | RectDef | CircleDef;
+
+/** A layer constitutes a group of commands. */
+interface Layer {
+  [namedCommand: string]: ShapeDef | ShapeDef & Stroked;
+}
+
+/** A friendly API for drawing shapes to a 2D-context */
+interface ShapeApi {
+  /**
+   * Draws a SVG path to the context.
+   * @param path The definition of the path to draw.
+   */
+  path(path: PathDef | PathDef & Stroked): void;
+
+  /**
+   * Draws a polygon to the context.
+   * @param poly The definition of the polygon to draw.
+   */
+  poly(poly: PolyDef | PolyDef & Stroked): void;
+
+  /**
+   * Draws a rectangle to the context.
+   * @param def The definition of the rectangle to draw.
+   */
+  rect(def: RectDef & Stroked): void;
+
+  /**
+   * Writes the provided text to the context.
+   * @param def The definition of the text to draw.
+   */
+  text(def: TextDef): void;
+
+  /**
+   * Measures the width of the text in whole pixels, rounding up.
+   * @param text The text to measure.
+   * @param font The font to use when measuring.
+   */
+  textWidth(text: string, font: string): void;
+
+  /**
+   * Draws the provided layers to the context.
+   * 
+   * **Usage:**
+    ```
+    shape.layers([
+      {
+        'rect background': {
+          x: 0, y: 10,
+          width: 10px,
+          height: 20px
+          style: '#222'
+        },
+        'rect background-border': {
+          x: 0, y: 10,
+          width: 10px,
+          height: 20px
+          style: '#fff',
+          thickness: 2
+        },
+        'text contents': {
+          x: 10, y: 15,
+          value: 'Hi!',
+          font: '10px Arial',
+          style: '#fff'
+        }
+      }
+    ]);
+    ```
+   * @param layers The layers to draw
+   */
+  layers(layers: Layer[]);
+
+  /**
+   * Prepends an offset to the provided `pathString`,
+   * as defined by `x` and `y`.
+   *
+   * **Note:** In order for the path to be properly offset,
+   * **all commands in the path must be relative.**
+   * @param pathString An all-relative path string
+   * @param x Number of pixels offset from the left.
+   * @param y Number of pixels offset from the top.
+   */
+  offsetPath(pathString: string, x?: number, y?: number): string;
+}
+
+/** Provides a friendly API for drawing to a 2D-context */
+declare function shapes(ctx: CanvasRenderingContext2D): ShapeApi;
+
+export = shapes;
---
+++ src/shapes.js
@@ -0,0 +1,125 @@
+(() => {
+  function shapes(target) {
+    const ctx = target.tagName === 'CANVAS' ? ctx.getContext('2d') : target;
+
+    let offsX = 0;
+    let offsY = 0;
+
+    const setCache = Object.create(null);
+
+    const set = (prop, value) =>
+      setCache[prop] !== value ? (ctx[prop] = setCache[prop] = value) : void 0;
+
+    const drawPoly = points => {
+      ctx.beginPath();
+      ctx.moveTo(offsX + points[0][0], offsY + points[0][1]);
+
+      for (let i = 1; i < points.length; i++) {
+        ctx.lineTo(offsX + points[i][0], offsY + points[i][1]);
+      }
+    };
+
+    const getStyleType = isStroked => (isStroked ? 'strokeStyle' : 'fillStyle');
+
+    const setStyle = (style, thickness) => {
+      set(getStyleType(thickness), style);
+      thickness && set('lineWidth', thickness);
+    };
+
+    const getLayerCommand = name => {
+      const end = name.indexOf(' ');
+
+      return end !== -1 ? name.substr(0, end) : name;
+    };
+
+    class ShapesApi {
+      path({ path, style, thickness }) {
+        setStyle(style, thickness);
+
+        if (thickness) {
+          ctx.stroke(path);
+        } else {
+          ctx.fill(path);
+        }
+      }
+
+      poly({ points, style, thickness }) {
+        setStyle(style, thickness);
+
+        drawPoly(points);
+
+        if (thickness) {
+          ctx.stroke();
+        } else {
+          ctx.fill();
+        }
+      }
+
+      circle({ x, y, radius, style, thickness }) {
+        setStyle(style, thickness);
+
+        ctx.beginPath();
+        ctx.arc(offsX + x, offsY + y, radius, 0, 2 * Math.PI);
+
+        if (thickness) {
+          ctx.stroke();
+        } else {
+          ctx.fill();
+        }
+      }
+
+      text({ x, y, value, font, style }) {
+        set('font', font);
+        set('fillStyle', style);
+
+        ctx.fillText(value, offsX + x, offsY + y);
+      }
+
+      rect({ x, y, width, height, style, thickness }) {
+        setStyle(style, thickness);
+
+        if (thickness) {
+          ctx.strokeRect(offsX + x, offsY + y, width, height);
+        } else {
+          ctx.fillRect(offsX + x, offsY + y, width, height);
+        }
+      }
+
+      textWidth(value, font) {
+        set('font', font);
+
+        return Math.ceil(ctx.measureText(value).width);
+      }
+
+      layers(layers) {
+        layers.filter(l => !!l).forEach(layer => {
+          for (const name in layer) {
+            this[getLayerCommand(name)](layer[name]);
+          }
+        });
+      }
+
+      setOrigin(x, y) {
+        offsX = Math.ceil(x);
+        offsY = Math.ceil(y);
+      }
+
+      offset(x, y) {
+        offsX += Math.ceil(x);
+        offsY += Math.ceil(y);
+      }
+
+      offsetPath(pathString, x, y) {
+        return `M${offsX + x || 0} ${offsY + y || 0} ` + pathString;
+      }
+    }
+
+    return new ShapesApi();
+  }
+
+  if (typeof module !== 'undefined') {
+    module.exports = shapes;
+  } else {
+    window.shapes = shapes;
+  }
+})();
