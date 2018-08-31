d7ee130b1d8be3f3528a3783cfa94a09234e40d0
mausworks <rasmuswennerstrom@gmail.com>
2018-08-31 15:33:03 +0200

    feat: Add roundedRect to the shapes API

--- src/shapes.js
+++ src/shapes.js
@@ -67,6 +67,33 @@ const shapes = target => {
       }
     }
 
+    roundedRect = ({ x, y, width, height, radius, style, thickness }) => {
+      ctx.beginPath();
+      ctx.moveTo(x + radius, y);
+      ctx.lineTo(x + width - radius, y);
+      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+      ctx.lineTo(x + width, y + height - radius);
+      ctx.quadraticCurveTo(
+        x + width,
+        y + height,
+        x + width - radius,
+        y + height
+      );
+      ctx.lineTo(x + radius, y + height);
+      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+      ctx.lineTo(x, y + radius);
+      ctx.quadraticCurveTo(x, y, x + radius, y);
+      ctx.closePath();
+
+      setStyle(style, thickness);
+
+      if (thickness) {
+        ctx.stroke(stroke);
+      } else {
+        ctx.fill(fill);
+      }
+    };
+
     text({ x, y, value, font, style, baseLine }) {
       set('font', font);
       set('fillStyle', style);
